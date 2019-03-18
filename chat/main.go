36e19bf83b4cd3dca44765ccba13cd081934b439
NanairoMegane <48196818+NanairoMegane@users.noreply.github.com>
2019-03-18 13:10:11 +0900

    Update main.go

--- chat/main.go
+++ chat/main.go
@@ -76,9 +76,8 @@ func main() {
 	/* Gomniauth の設定 */
 	gomniauth.SetSecurityKey("chattest3594")
 	gomniauth.WithProviders(
-		google.New("405254153423-ueimm9ll44q24s00ctrvcn1v2s1j9su5.apps.googleusercontent.com",
-			"o5lSpqQLVHv8NAk4ok1cCwRy",
-			"http://localhost:8080/auth/callback/google",
+		google.New("yyy",
+			"xxx",
 		),
 	)
 
