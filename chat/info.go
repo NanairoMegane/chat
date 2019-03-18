c2033e0b695453cc69bc5af106572dee72c01e02
NanairoMegane <kobarengaruda@gmail.com>
2019-03-18 13:31:51 +0900

    ignore3

--- chat/info.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const (
-	SecurityKey    = "chattest3594"
-	ClientId       = "405254153423-ueimm9ll44q24s00ctrvcn1v2s1j9su5.apps.googleusercontent.com"
-	ClientSecurity = "o5lSpqQLVHv8NAk4ok1cCwRy"
-)
